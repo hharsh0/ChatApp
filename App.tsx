b57a6cffd4709a2366615c54603201007059fbb1
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2022-09-03 19:00:00 +0530

    connection to aws backend and login setup

--- App.tsx
+++ App.tsx
@@ -4,8 +4,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+import {withAuthenticator} from 'aws-amplify-react-native'
 
-export default function App() {
+Amplify.configure(config)
+
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -20,3 +25,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App)
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "chatApp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "chatApp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
